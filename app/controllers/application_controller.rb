f63c07aadc171bef1b94b2a983784d229420ab7c
38752 <118719220+38752@users.noreply.github.com>
2022-11-26 15:07:47 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  # ログインしていない場合は自動的にログインのパスに飛ばされる(新規登録ページには遷移できる)
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
